76b2adb9d8ed542aeb938d48df90be300bbd9f65
Oleg Avdeev <falcon.rawbyte@gmail.com>
2020-02-04 01:35:04 +0300

    Basic backend structure
    github listener server for webhooks
    
    Added repo refresh script to the listener server
    
    Testing webhook parse
    
    Test commit
    
    Fixed json parsing
    
    Fixed refresh script
    
    Test commit
    
    Fixed error in server script
    
    Test commit
    
    Server should be ready to parse and handle github webhooks
    
    Webhook response
    
    More information added to the web response
    
    Fixed sh call
    
    Fixes exec paths
    
    Hardcoded path
    
    More fixes...
    
    Script improvement
    
    sh fix
    
    echo, not cat...
    
    What...
    
    Added var command
    
    Slow descend into madness...
    
    Example code
    
    More stolen code
    
    ???
    
    Added sh to execute script
    
    Added stderr -> stdout
    
    Testing exec
    
    Continue to struggle
    
    Using spawn
    
    child
    
    bash
    
    script
    
    1
    
    pgrep pkill
    
    server fixes
    
    Fixed sh
    
    ???
    
    Maybe?
    
    Debug command
    
    Script — more info

---
+++ refresh-repo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# $1 — branch name
+echo "Script: $0"
+echo "Branch: $1"
+
+echo "Changing directory to /home/$1"
+cd "/home/$1"
+
+echo "Checking for new commits:"
+if git reset --hard ; then
+    git pull -f
+    
+    cd "/home/"
+    echo "Killing running server (If exists)"
+    if pgrep "$1"; then pkill "$1"; fi
+    echo "Restarting server"
+    nohup node "$1/server" > "../$1.log" 2>&1 &
+fi
--- server.js
+++ server.js
@@ -1,115 +1,48 @@
-/*server.js*/
-var port = 8000;
-var serverUrl = "127.0.0.1";
+const http = require('http');
+const { parse } = require('querystring');
+const { exec } = require('child_process');
 
-var WebSocket = require('ws')
-var http = require("http");
-var path = require("path");
-var fs = require("fs");
-
-var checkMimeType = true;
-
-console.log("Starting web server at " + serverUrl + ":" + port);
-
-http.createServer(function (req, res) {
-
-    var now = new Date();
-
-    var filename = req.url || "pages/ws-desk.html";
-    var ext = path.extname(filename);
-    var localPath = __dirname;
-    var validExtensions = {
-        ".html": "text/html",
-        ".js": "application/javascript",
-        ".css": "text/css",
-        ".txt": "text/plain",
-        ".jpg": "image/jpeg",
-        ".gif": "image/gif",
-        ".png": "image/png",
-        ".woff": "application/font-woff",
-        ".woff2": "application/font-woff2"
-    };
-
-    var validMimeType = true;
-    var mimeType = validExtensions[ext];
-    if (checkMimeType) {
-        validMimeType = validExtensions[ext] != undefined;
-    }
-
-    if (validMimeType) {
-        localPath += filename;
-        fs.exists(localPath, function (exists) {
-            if (exists) {
-                console.log("Serving file: " + localPath);
-                getFile(localPath, res, mimeType);
-            } else {
-                console.log("File not found: " + localPath);
-                res.writeHead(404);
-                res.end();
-            }
-        });
-
-    } else {
-        console.log("Invalid file extension detected: " + ext + " (" + filename + ")")
-    }
-}).listen(port, serverUrl);
-
-function getFile(localPath, res, mimeType) {
-    fs.readFile(localPath, function (err, contents) {
-        if (!err) {
-            res.setHeader("Content-Length", contents.length);
-            if (mimeType != undefined) {
-                res.setHeader("Content-Type", mimeType);
-            }
-            res.statusCode = 200;
-            res.end(contents);
-            initializeWS();
-        } else {
-            res.writeHead(500);
-            res.end();
-        }
-    });
-}
-
-var boop_array = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-
-function initializeWS() {
-    
-    const wss = new WebSocket.Server({ port: 8088 })
-    
-    wss.on('connection', ws => {
-    
-        ws.on('message', message => {
-            console.log(`Received message => ${message}`)
+const server = http.createServer((req, res) => {
+    if (req.method === 'POST') {
+        collectRequestData(req, result => {
             
-            var data = JSON.parse(message);
-            if (data[0]=='boop')
-            {
-                var index = data[1]*8 + data[2];
-                updateBoop(index);
-                broadcastBoops(wss, 'update');
-            }
-        })
-        
-        var connection_data = ['connection', boop_array];
-        ws.send(JSON.stringify(connection_data));
-    });
-    
-    wss.on('error', message => {
-        console.log(`Error occured => ${message}`)
-    })
-}
+            console.log("Incoming webhook detected. Parsing");
+            const branch = result["ref"].split("/").pop();
+            const command = 'sh';
+            
+            // const child = exec(command, ['sh /home/github-listener/refresh-repo.sh github-listener', branch, 'branch']);
+            const child = exec('sh /home/github-listener/refresh-repo.sh github-listener');
 
-function broadcastBoops(wss, event) {
-    var boop_info = JSON.stringify([event, boop_array]);
-    wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(boop_info);
-        }
-    });
-}
+            child.on('exit', code => {
+                res.end(`Exit code is: ${code}`);
+            });
 
-function updateBoop(index) {
-    boop_array[index] = (boop_array[index] + 1) % 3;
-}
+            child.stdout.on('data', (data) => {
+                console.log(data);
+            });
 
+            child.stderr.on('data', (data) => {
+                console.log(data);
+            });
+            
+        });
+    } 
+    else {
+        res.end(`Argument server is doing ok... How are you?`);
+    }
+});
+server.listen(9000);
+
+function collectRequestData(request, callback) {
+    const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+    if(request.headers['content-type'] === FORM_URLENCODED) {
+        request.on('data', chunk => {
+		var p = parse(chunk.toString());
+		var json = JSON.parse(p["payload"]);
+		callback(json);
+        });
+    }
+    else {
+        callback(null);
+    }
+}
