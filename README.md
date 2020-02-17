03f1e2b23779b4a506e9a4a07a71ba707b4cb668
Oleg Avdeev <falcon.rawbyte@gmail.com>
2020-02-16 23:11:04 -0500

    +README +new routes

---
+++ README.md
@@ -0,0 +1,17 @@
+REST API server running on node + express + mongodb. <br>
+Current endpoints:
+
+- `/api/arguments`: GET, POST
+- `/api/arguments/{id}`: GET, UPDATE, DELETE
+
+- `/api/characters`: GET, POST
+- `/api/characters/{id}`: GET, UPDATE, DELETE
+
+- `/api/desks`: GET, POST
+- `/api/desks/{id}`: GET, UPDATE, DELETE
+
+- `/api/modules`: GET, POST
+- `/api/modules/{id}`: GET, UPDATE, DELETE
+
+- `/api/users`: GET, POST
+- `/api/users/{id}`: GET, UPDATE, DELETE
--- server.js
+++ server.js
@@ -2,9 +2,14 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
-  users = require('./api/users/models/users.model'),
   bodyParser = require('body-parser');
-  
+
+require('./api/arguments/models/model');
+require('./api/characters/models/model');
+require('./api/desks/models/model');
+require('./api/modules/models/model');
+require('./api/users/models/model');
+
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/argumentdb'); 
@@ -14,8 +19,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-var routes = require('./api/users/routes/users.routes'); //importing route
-routes(app); //register the route
+(require('./api/arguments/routes/routes')(app));
+(require('./api/characters/routes/routes')(app));
+(require('./api/desks/routes/routes')(app));
+(require('./api/modules/routes/routes')(app));
+(require('./api/users/routes/routes')(app));
 
 
 app.listen(port);
