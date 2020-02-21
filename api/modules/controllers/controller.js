85a7b78c31ac7a22d09104280e721d07c1d88f4b
Oleg Avdeev <falcon.rawbyte@gmail.com>
2020-02-21 17:21:14 -0500

    Module-users api

--- api/modules/controllers/controller.js
+++ api/modules/controllers/controller.js
@@ -1,12 +1,15 @@
 var mongoose = require('mongoose'),
-    model = mongoose.model('module');
+    model = mongoose.model('module'),
+    module_users = mongoose.model('module_users');
 
 exports.list = function (req, res) {
     model.find({}, handler(res));
 }
 
 exports.create = function (req, res) {
-    (new model(req.body)).save(handler(res));
+    var module = (new model(req.body)).save(handler(res));
+    var admin = { user_id : req.body.user_id, module_id : module._id, permissions : "owner" };
+    (new module_users(admin)).save(handler(res));
 }
 
 exports.get = function (req, res) {
@@ -21,6 +24,13 @@ exports.delete = function (req, res) {
     model.findOneAndDelete(req.params.id, handler(res));
 }
 
+exports.getPlayers = function (req, res) {
+    module_users.find({}, handler(res));
+}
+
+exports.invite = function (req, res) {
+    (new module_users(req.body)).save(handler(res));
+}
 
 function handler(res) {
     return (err, user) => {
--- api/modules/routes/routes.js
+++ api/modules/routes/routes.js
@@ -9,4 +9,8 @@ module.exports = function (app) {
         .get(controller.get)
         .put(controller.update)
         .delete(controller.delete);
+
+    app.route('/modules/users')
+        .get(controller.getPlayers)
+        .put(controller.invite);
 };
